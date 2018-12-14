f29e9f85a70bd69d56e099bb569fc21a3a9f1638
Ahmed Abdelhamid <slevin.glevra@gmail.com>
2018-12-14 05:45:50 +0300

    Add button to contact page

--- src/components/HomePage.js
+++ src/components/HomePage.js
@@ -1,10 +1,15 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
+import { Button } from 'reactstrap';
 
 const HomePage = () => (
   <div className="homepage d-flex align-items-center">
     <div className="container text-white text-center">
       <h1>Website Title</h1>
       <p>Welcome to our website</p>
+      <Button tag={Link} to="/contact" size="lg">
+        Contact Us
+      </Button>
     </div>
   </div>
 );
