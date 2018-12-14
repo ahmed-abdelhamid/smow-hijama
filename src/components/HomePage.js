f56fc6d892420662f333778f0ef13cc3a8c4a67f
Ahmed Abdelhamid <slevin.glevra@gmail.com>
2018-12-14 04:46:36 +0300

    Style home page

--- src/components/HomePage.js
+++ src/components/HomePage.js
@@ -1,3 +1,10 @@
 import React from 'react';
 
-export default () => <div>Home Page</div>;
+export default () => (
+  <div className="homepage d-flex align-items-center">
+    <div className="container text-white text-center">
+      <h1>Website Title</h1>
+      <p>Welcome to our website</p>
+    </div>
+  </div>
+);
