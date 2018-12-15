31e02379bf6f15815c48582bfd93db76c1354c49
Ahmed Abdelhamid <slevin.glevra@gmail.com>
2018-12-16 02:24:28 +0300

    Add google map

--- src/components/ContactUs.js
+++ src/components/ContactUs.js
@@ -1,12 +1,19 @@
 import React, { Component } from 'react';
 
 import Slider from './Slider';
+import Location from './Location';
 
 export default class ContactUs extends Component {
   render() {
     return (
       <div>
         <Slider />
+        <div className="container">
+          <div className="row text-center">
+            <Location />
+            <div className="col-md-6">Address Details</div>
+          </div>
+        </div>
       </div>
     );
   }
