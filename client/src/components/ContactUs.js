242f833d7fb946e55e979549ba38fbee7cc09ed0
Ahmed Abdelhamid <slevin.glevra@gmail.com>
2018-12-31 16:08:51 +0300

    Localize title

--- client/src/components/ContactUs.js
+++ client/src/components/ContactUs.js
@@ -1,18 +1,36 @@
 import React, { Component } from 'react';
 import { Container, Row, Col } from 'reactstrap';
+import { withLocalize, Translate } from 'react-localize-redux';
+import { renderToStaticMarkup } from 'react-dom/server';
 
 import Slider from './Slider';
 import Location from './Location';
 import ContactForm from './FormComponents/ContactForm';
 import SocialMediaLinks from './SocialMediaLinks';
+import translations from '../utils/translations/contact-us.json';
 
-export default class ContactUs extends Component {
+class ContactUs extends Component {
+  constructor(props) {
+    super(props);
+
+    this.props.initialize({
+      languages: [
+        { name: 'العربية', code: 'ar' },
+        { name: 'English', code: 'en' }
+      ],
+      options: { renderToStaticMarkup }
+    });
+    this.props.addTranslation(translations);
+  }
+
   render() {
     return (
       <div className="contact text-center">
         <Container>
           <Slider />
-          <h2 className="mb-4">Contact Us</h2>
+          <h2 className="mb-4 contact__title">
+            <Translate id="TITLE" />
+          </h2>
           <Row>
             <Col md="6">
               <Location />
@@ -29,3 +47,5 @@ export default class ContactUs extends Component {
     );
   }
 }
+
+export default withLocalize(ContactUs);
