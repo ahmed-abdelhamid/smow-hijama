d3af7dca15fde99b8dea83e113a269f97660b258
Ahmed Abdelhamid <slevin.glevra@gmail.com>
2019-01-01 01:05:59 +0300

    Adjust Slider

--- client/src/components/Slider.js
+++ client/src/components/Slider.js
@@ -1,5 +1,5 @@
-import React from 'react';
-import { UncontrolledCarousel } from 'reactstrap';
+import React, { Component } from 'react';
+import { Carousel, CarouselItem } from 'reactstrap';
 
 const items = [
   {
@@ -24,14 +24,63 @@ const items = [
   }
 ];
 
-const Slider = () => (
-  <UncontrolledCarousel
-    className="pb-3"
-    items={items}
-    indicators={false}
-    controls={false}
-    pause={false}
-  />
-);
+class Slider extends Component {
+  state = { activeIndex: 0 };
+
+  onExiting = () => {
+    this.animating = true;
+  };
+
+  onExited = () => {
+    this.animating = false;
+  };
+
+  next = () => {
+    if (this.animating) return;
+    const nextIndex =
+      this.state.activeIndex === items.length - 1
+        ? 0
+        : this.state.activeIndex + 1;
+    this.setState({ activeIndex: nextIndex });
+  };
+
+  previous = () => {
+    if (this.animating) return;
+    const nextIndex =
+      this.state.activeIndex === 0
+        ? items.length - 1
+        : this.state.activeIndex - 1;
+    this.setState({ activeIndex: nextIndex });
+  };
+
+  goToIndex = newIndex => {
+    if (this.animating) return;
+    this.setState({ activeIndex: newIndex });
+  };
+
+  render() {
+    const { activeIndex } = this.state;
+
+    const slides = items.map(item => (
+      <CarouselItem
+        onExiting={this.onExiting}
+        onExited={this.onExited}
+        key={item.src}
+      >
+        <img src={item.src} alt={item.altText} />
+      </CarouselItem>
+    ));
+
+    return (
+      <Carousel
+        activeIndex={activeIndex}
+        next={this.next}
+        previous={this.previous}
+      >
+        {slides}
+      </Carousel>
+    );
+  }
+}
 
 export default Slider;
