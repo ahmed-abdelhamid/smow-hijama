0daab42a7a73de0fd63e0cfe9daa45f69fa06c59
Ahmed Abdelhamid <slevin.glevra@gmail.com>
2019-01-22 11:05:12 +0300

    Separate slider images in fixutres folder

--- client/src/components/Slider.js
+++ client/src/components/Slider.js
@@ -1,33 +1,12 @@
 import React from 'react';
 import { Carousel } from 'react-responsive-carousel';
 
-const items = [
-  {
-    src: '/assets/images/sm.jpg',
-    altText: 'Smow Al Hayat'
-  },
-  {
-    src: '/assets/images/123.jpg',
-    altText: 'Smow Al Hayat'
-  },
-  {
-    src: '/assets/images/3.jpg',
-    altText: 'Smow Al Hayat'
-  },
-  {
-    src: '/assets/images/4.jpg',
-    altText: 'Smow Al Hayat'
-  },
-  {
-    src: '/assets/images/5.jpg',
-    altText: 'Smow Al Hayat'
-  }
-];
+import { IMAGES } from '../fixtures/sliderImages';
 
 const Slider = () => (
   <div>
     <Carousel showStatus={false} infiniteLoop autoPlay>
-      {items.map(({ src, altText }) => (
+      {IMAGES.map(({ src, altText }) => (
         <div key={src}>
           <img src={src} alt={altText} />
         </div>
---
+++ client/src/fixtures/sliderImages.js
@@ -0,0 +1,22 @@
+export const IMAGES = [
+  {
+    src: '/assets/images/sm.jpg',
+    altText: 'Smow Al Hayat'
+  },
+  {
+    src: '/assets/images/123.jpg',
+    altText: 'Smow Al Hayat'
+  },
+  {
+    src: '/assets/images/3.jpg',
+    altText: 'Smow Al Hayat'
+  },
+  {
+    src: '/assets/images/4.jpg',
+    altText: 'Smow Al Hayat'
+  },
+  {
+    src: '/assets/images/5.jpg',
+    altText: 'Smow Al Hayat'
+  }
+];
